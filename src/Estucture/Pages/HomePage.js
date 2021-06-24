eb642329718a077363dea9d8d8779ef615c033f5
LuisEduardoFrias <54819225+LuisEduardoFrias@users.noreply.github.com>
2021-06-24 15:54:24 -0400

    loginPage

--- src/Estucture/Pages/HomePage.js
+++ src/Estucture/Pages/HomePage.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import backgroud from './background.png';
 
 export default function HomePage()
 {
     return (
-        <div>
-            Holas a todos
+        <div style={{ display:'flex', alignItems:'center', justifyContent: 'center', height:'100%', width:'100%'}}>
+            <img src={backgroud} style={{height:'100%', width:'100%'}} alt="Imagen de eps" />
         </div>
     )
 }
